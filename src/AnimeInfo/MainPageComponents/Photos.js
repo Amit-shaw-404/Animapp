ae332f09b13716d2a4165bf5876af6d3e0089661
AmitShaw <yorsamit1@gmail.com>
2021-05-10 01:24:35 +0530

    Added photos page

---
+++ src/AnimeInfo/MainPageComponents/Photos.js
@@ -0,0 +1,53 @@
+import {
+  GridList,
+  GridListTile,
+  useMediaQuery,
+  useTheme
+} from "@material-ui/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+export default function Photos() {
+  const [photos, setPhotos] = useState([]);
+  const getId = (path) => {
+    let i = 12,
+      l = 11;
+    while (path[i] !== "/") {
+      i++;
+    }
+    return path.substring(l, i);
+  };
+  const history = useHistory();
+  const path = history.location.pathname;
+  const id=getId(path);
+  useEffect(() => {
+    const request = async () => {
+      const pics = await axios.get(
+        `https://api.jikan.moe/v3/anime/${id}/pictures`
+      );
+      setPhotos(pics.data.pictures);
+    };
+    request();
+  }, []);
+  const theme = useTheme();
+  const small = useMediaQuery(theme.breakpoints.only("sm"));
+  const exsmall = useMediaQuery(theme.breakpoints.only("xs"));
+  return (
+    <GridList
+      cols={exsmall ? 2 : small ? 3 : 4}
+      cellHeight={250}
+      style={{ margin: "10px" }}
+    >
+      {photos !== undefined
+        ? photos.map((item, index) => (
+            <GridListTile key={index}>
+              <a href={item.large}>
+                <img src={item.large} alt={index} width="100%"/>
+              </a>
+            </GridListTile>
+          ))
+        : ""}
+    </GridList>
+  );
+}
